52e7d109c164a95823195f04726c87a0a2c4ea95
saihari001 <hariprasanth3196@gmail.com>
2024-02-26 21:05:58 +0530

    added netlify form

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -4,13 +4,21 @@ import "./ContactForm.css";
 const ContactForm = () => {
   return (
     <div className='contact-form-container'>
-        <form name='contact' method='POST' data-netlify='true' onSubmit='submit'>
+        <form name='contact v1' method='POST' data-netlify='true' onSubmit='submit'>
             <div className='name-container'>
+              <label>First name <br />
                 <input type="text" name='firstname' placeholder='First Name'/>
+              </label>
+              <label>Last name <br />
                 <input type="text" name='Lastname' placeholder='Last Name'/>
+              </label>
             </div>
-            <input type="text" name='email' placeholder='Email'/>
-            <textarea type='text' name='message' placeholder='Message' rows={3} />
+            <label htmlFor="email">
+              <input type="text" name='email' id='email' placeholder='Email'/>
+            </label>
+            <label>Comments
+              <textarea type='text' name='message' placeholder='Message' rows={3} />
+            </label>
 
             <button type='submit'>SEND</button>
         </form>
