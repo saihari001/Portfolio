43665b98616c83ac8a552c62e19a17caf927b8e5
saihari001 <hariprasanth3196@gmail.com>
2024-02-26 21:22:22 +0530

    Removed netlify form

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -4,32 +4,15 @@ import "./ContactForm.css";
 const ContactForm = () => {
   return (
     <div className='contact-form-container'>
-        <form name='contact' netlify>
+        <form>
             <div className='name-container'>
-              <p>
-              <label>First name <br />
                 <input type="text" name='firstname' placeholder='First Name'/>
-              </label>
-              </p>
-              <p>
-              <label>Last name <br />
                 <input type="text" name='Lastname' placeholder='Last Name'/>
-              </label>
-              </p>
             </div>
-            <p>
-            <label htmlFor="email">
-              <input type="text" name='email' id='email' placeholder='Email'/>
-            </label>
-            </p>
-            <p>
-            <label>Comments
-              <textarea type='text' name='message' placeholder='Message' rows={3} />
-            </label>
-            </p>
-            <p>
-            <button type='submit'>SEND</button>
-            </p>
+            <input type="text" name='email' placeholder='Email'/>
+            <textarea type='text' name='message' placeholder='Message' rows={3} />
+
+            <button>SEND</button>
         </form>
     </div>
   )
